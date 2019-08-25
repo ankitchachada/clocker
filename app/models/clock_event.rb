33893cf3e3459c3b7478de50777c5d42b38c2c19
ankit <ankitchachada@gmail.com>
2019-08-24 18:40:02 -0700

    timezone support added

--- app/models/clock_event.rb
+++ app/models/clock_event.rb
@@ -1,5 +1,8 @@
 class ClockEvent < ApplicationRecord
 	belongs_to :user
-	scope :today, lambda {where("DATE(created_at) = ?", Date.today)}
+	scope :today, lambda {
+		now = Time.now
+		where("created_at between :start and :end", :start => now.beginning_of_day, :end   => now.end_of_day)
+	}
 	scope :desc, lambda {order("created_at desc")}
 end
